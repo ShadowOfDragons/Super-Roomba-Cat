e547f41b9389c23f8f240d8f1c60cc8e8a555730
Zoé González Requejo <zgonreq@gmail.com>
2018-03-05 14:03:17 +0100

    Added destroyer y spawners (still need testing)

---
+++ Assets/Scripts/DestroyerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerBehaviour : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object Enter!");
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviour : MonoBehaviour
+{
+    public GameObject[] obj;
+
+    void Spawn()
+    {
+        Instantiate(obj[Random.Range(0, obj.GetLength(0))], transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Spawn();
+    }
+}
