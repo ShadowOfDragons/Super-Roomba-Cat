0d7189170431d8e47a37fc8d242a552157b9875f
Zoé González Requejo <zgonreq@gmail.com>
2018-03-06 12:27:25 +0100

    Lvl Prefabs logic
    *Prefab movement
    *Prefab spawn
    *Prefab destruction
    *Simple collisions working

--- Assets/Scripts/DestroyerBehaviour.cs
+++ Assets/Scripts/DestroyerBehaviour.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class DestroyerBehaviour : MonoBehaviour
 {
+    public GameObject[] lvlPrefab;
+    public Vector3 prefabCoord = new Vector3 (0, 0, 0);
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object Enter!");
+        prefabCoord = new Vector3(60, 0, 0);
+        GameObject.Instantiate(lvlPrefab[Random.Range(0, lvlPrefab.GetLength(0))], prefabCoord, Quaternion.identity);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
         Destroy(other.gameObject);
     }
 }
---
+++ Assets/Scripts/LvlMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlMovement : MonoBehaviour
+{
+    public Transform lvlTransform;
+    public Vector3 lvlMovment;
+    public float lvlSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lvlMovment = lvlTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lvlMovment.x = lvlMovment.x + lvlSpeed;
+        lvlTransform.position = lvlMovment;
+	}
+}
