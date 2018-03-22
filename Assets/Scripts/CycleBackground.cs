90076842623b23f8fac6a39e2b60b5b45b070481
Zoé González Requejo <zgonreq@gmail.com>
2018-03-22 19:06:39 +0100

    Several improvements
    * Added enemies
    * Added cyling backgrond on title and ending screen
    * Changed platform spawn positions
    * Tweeked platform and background speeds
    * Added testing bulid

---
+++ Assets/Scripts/CycleBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleBackground : MonoBehaviour
+{
+    [Header("Background")]
+    public GameObject[] lvlBackground;
+    public Vector3 backgroundPos;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject.Instantiate(lvlBackground[Random.Range(0, lvlBackground.GetLength(0))], backgroundPos, Quaternion.identity);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
